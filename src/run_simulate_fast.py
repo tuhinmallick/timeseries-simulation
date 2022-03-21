62eb5f73a657d9daadc5ff746b4e2c6544445b79
Tuhin Mallick <67542764+tuhinmallick@users.noreply.github.com>
2022-03-21 12:03:01 +0100

    Update run_simulate_fast.py

--- src/run_simulate_fast.py
+++ src/run_simulate_fast.py
@@ -61,6 +61,22 @@ st.markdown(f""" <style>
         padding-left: {padding}rem;
         padding-bottom: {padding}rem;
     }} </style> """, unsafe_allow_html=True)
+st.markdown("""
+        <style>
+               .css-18e3th9 {
+                    padding-top: 0rem;
+                    padding-bottom: 10rem;
+                    padding-left: 5rem;
+                    padding-right: 5rem;
+                }
+               .css-1d391kg {
+                    padding-top: 3.5rem;
+                    padding-right: 1rem;
+                    padding-bottom: 3.5rem;
+                    padding-left: 1rem;
+                }
+        </style>
+        """, unsafe_allow_html=True)
 # import pdb;pdb.set_trace()
 # if st.session_state['logged'] == False:
 forecasty_image = Image.open(os.path.join(artifact_location, "logo", "logo_forecasty.PNG"))
